712d618b58d4b5bfad77b3605167567376a34d03
Woodworth <woodworthcommunications@gmail.com>
2015-01-10 18:26:30 -0600

    Finished address coordinates assignment

--- address_to_coords.rb
+++ address_to_coords.rb
@@ -17,24 +17,49 @@ url_safe_address = URI.encode(the_address)
 
 # Your code goes below. Hints:
 
- url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address=" + "#{url_safe_address}"
+url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address=" + "#{url_safe_address}"
 
-raw_data = "#{url_of_data_we_want}"
+raw_data = open(url_of_data_we_want).read
 
 # -- Checking to make sure request formatting works --
 # puts url_of_data_we_want
-# puts "#{raw_data}"
+# puts raw_data.class
+# puts "<=================================>"
+# puts raw_data.length
+# puts "<=================================>"
+# puts raw_data
 
-# parsed_data = ???
+parsed_data = JSON.parse(raw_data)
 
-# ...
+# puts parsed_data.class
+# puts parsed_data.keys
+
+results = parsed_data["results"]
+
+# puts results.class
+# puts results.length
+# puts results
+
+first = results[0]
+
+# puts first.class
+# puts first.keys
+
+geometry = first["geometry"]
+
+# puts geometry.class
+# puts geometry.keys
+
+location = geometry["location"]
+
+# puts location
 
 # Let's store the latitude in a variable called 'the_latitude',
 #   and the longitude in a variable called 'the_longitude'.
 
-# the_latitude = ???
-# the_longitude = ???
+the_latitude = location["lat"]
+the_longitude = location["lng"]
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The latitude of #{the_address} is #{the_latitude} and the longitude is #{the_longitude}."
+puts "The latitude of #{the_address} is #{the_latitude} and the longitude is #{the_longitude}."
